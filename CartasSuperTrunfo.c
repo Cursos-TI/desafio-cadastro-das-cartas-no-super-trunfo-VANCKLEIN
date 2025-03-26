a76a88e61e483a154182283ed466e4d96047d460
VANCKLEIN <programadorklein@gmail.com>
2025-03-26 17:19:46 +0000

    Inseri as variáveis (todas com inicialização) e publiquei os dados das duas cartas com o comando printf.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,38 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    char EstadoA01[50] = "Alagoas";
+    char CodigoA01[20] = "A01";
+    char CidadeA01[20] = "Maceió"; 
+    int PopulacaoA01 = 957;
+    int AreaA01 = 509; 
+    int PIBA01 = 4;
+    int TurismoA01 = 100;
 
+    printf("Código da Carta: %s\n", CodigoA01);
+    printf("Estado: %s\n", EstadoA01);
+    printf("Nome da Cidade: %s\n", CidadeA01);  
+    printf("População: %d mil\n", PopulacaoA01);
+    printf("Área: %d km²\n", AreaA01);
+    printf("PIB: %d bilhóes\n", PIBA01);
+    printf("Número de Pontos Turísticos: %d\n", TurismoA01);
+
+    char EstadoB02[50] = "Amazonas";
+    char CodigoB02[20] = "B02";
+    char CidadeB02[20] =  "Manaus"; 
+    int  PopulacaoB02 = 2;
+    int AreaB02 = 11401; 
+    int PIBB02 = 103;
+    int TurismoB02 = 50;
+
+    printf("Código da Carta: %s\n", CodigoB02);
+    printf("Estado: %s\n", EstadoB02);
+    printf("Nome da Cidade: %s\n", CidadeB02);
+    printf("População: %d mihões\n", PopulacaoB02);
+    printf("Área: %d km²\n", AreaB02);
+    printf("PIB: %d\ bilhões\n", PIBB02);
+    printf("Número de Pontos Turísticos: %d\n", TurismoB02);
+    
     return 0;
 }
