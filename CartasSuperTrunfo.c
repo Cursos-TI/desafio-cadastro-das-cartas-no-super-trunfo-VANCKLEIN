70be94887ad62bf4f62d1b9794fb4ee006bebd29
VANCKLEIN <programadorklein@gmail.com>
2025-03-29 11:10:40 +0000

    Comparação entre as cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,34 +21,70 @@ int main() {
     char EstadoA01[50] = "Alagoas";
     char CodigoA01[20] = "A01";
     char CidadeA01[20] = "Maceió"; 
-    int PopulacaoA01 = 957;
+    unsigned int PopulacaoA01 = 957000;
     int AreaA01 = 509; 
-    int PIBA01 = 4;
+    int PIBA01 = 4000000;
     int TurismoA01 = 100;
+
+    float desindidadeA01 = (float) PopulacaoA01 / AreaA01;
+
+    float pibpercapitaA01 = (float) PIBA01 / PopulacaoA01;
+
+    double superpoderA01 = (double) PopulacaoA01 + AreaA01 + PIBA01 + TurismoA01 + pibpercapitaA01 - desindidadeA01;
 
     printf("Código da Carta: %s\n", CodigoA01);
     printf("Estado: %s\n", EstadoA01);
     printf("Nome da Cidade: %s\n", CidadeA01);  
-    printf("População: %d mil\n", PopulacaoA01);
+    printf("População: %d\n", PopulacaoA01);
     printf("Área: %d km²\n", AreaA01);
-    printf("PIB: %d bilhóes\n", PIBA01);
+    printf("PIB: %d de reais\n", PIBA01);
     printf("Número de Pontos Turísticos: %d\n", TurismoA01);
+    printf("Densidade Populacional: %.3f hab/km²\n", desindidadeA01);
+    printf("PIB per Capita: %.3f reias\n", pibpercapitaA01);
+    printf("Super Poder: %f\n", superpoderA01);
+
+    printf("\n");
+    printf("\n");
 
     char EstadoB02[50] = "Amazonas";
     char CodigoB02[20] = "B02";
     char CidadeB02[20] =  "Manaus"; 
-    int  PopulacaoB02 = 2;
+    unsigned int  PopulacaoB02 = 2000000;
     int AreaB02 = 11401; 
-    int PIBB02 = 103;
+    int PIBB02 = 103000000;
     int TurismoB02 = 50;
+
+    float desindidadeB02 = (float) PopulacaoB02 / AreaB02;
+
+    float pibpercapitaB02 = (float) PIBB02 / PopulacaoB02;
+
+    double superpoderB02 = (double) PopulacaoB02 + AreaB02 + PIBB02 + TurismoB02 + pibpercapitaB02 - desindidadeB02;
 
     printf("Código da Carta: %s\n", CodigoB02);
     printf("Estado: %s\n", EstadoB02);
     printf("Nome da Cidade: %s\n", CidadeB02);
-    printf("População: %d mihões\n", PopulacaoB02);
+    printf("População: %d\n", PopulacaoB02);
     printf("Área: %d km²\n", AreaB02);
-    printf("PIB: %d\ bilhões\n", PIBB02);
+    printf("PIB: %d de reais\n", PIBB02);
     printf("Número de Pontos Turísticos: %d\n", TurismoB02);
+    printf("Densidade Populacional: %.3f hab/km²\n", desindidadeB02);
+    printf("PIB per Capita: %.3f reias\n", pibpercapitaB02);
+    printf("Super Poder: %f\n", superpoderB02);
+
+    
+    printf("\n");
+    printf("\n");
+
+    printf("Comparação entre as cartas!\n");
+    printf("\n");
+
+    printf("População: Carta 1 venceu (%d)\n", PopulacaoA01 > PopulacaoB02);
+    printf("Área: Carta 1 venceu (%d)\n", AreaA01 > AreaB02);
+    printf("PIB: Carta 1 venceu (%d)\n", PIBA01 > PIBB02);
+    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", TurismoA01 > TurismoB02);
+    printf("Densidade Populacional: Carta 1 venceu (%d)\n", -desindidadeA01 > -desindidadeB02); //ao invés de colocar sinal de menor (<), negativei os números
+    printf("PIB per Capita: Carta 1 venceu (%d)\n", pibpercapitaA01 > pibpercapitaB02);
+    printf("Super Poder: Carta 1 venceu (%d)\n", superpoderA01 > superpoderB02);
     
     return 0;
 }
